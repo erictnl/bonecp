d67e373947d725af2b6a5e0daee73a57e4075a15
Eric Jensen <ejensen@truenorthlogic.com>
2012-09-26 18:52:18 -0600

    Uncomment JDK 7 section for my purposes.

--- bonecp-test-commons/src/main/java/com/jolbox/bonecp/MockJDBCDriver.java
+++ bonecp-test-commons/src/main/java/com/jolbox/bonecp/MockJDBCDriver.java
@@ -1,19 +1,15 @@
 /**
- *  Copyright 2010 Wallace Wadge
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2010 Wallace Wadge
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 /**
  * 
  */
@@ -25,163 +21,172 @@ import java.sql.DriverManager;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
 import java.util.Properties;
-/* #ifdef JDK7
+// #ifdef JDK7
 import java.sql.SQLFeatureNotSupportedException;
 import java.util.logging.Logger;
-#endif JDK7 */
 
-/** A Fake jdbc driver for mocking purposes.
+// #endif JDK7
+/**
+ * A Fake jdbc driver for mocking purposes.
  * @author Wallace
- *
  */
 public class MockJDBCDriver implements Driver {
-	/** Connection handle to return. */
-	private volatile Connection connection = null;
-	/** called to return. */
-	private volatile MockJDBCAnswer mockJDBCAnswer;
-	/** on/off setting. */
-	private volatile static boolean active;
+    /** Connection handle to return. */
+    private volatile Connection connection = null;
+    /** called to return. */
+    private volatile MockJDBCAnswer mockJDBCAnswer;
+    /** on/off setting. */
+    private volatile static boolean active;
 
-	/**
-	 * Default constructor
-	 * @throws SQLException 
-	 */
-	public MockJDBCDriver() throws SQLException{
-		// default constructor
-		DriverManager.registerDriver(this);
-		this.active = true;
-	}
+    /**
+     * Default constructor
+     * @throws SQLException
+     */
+    public MockJDBCDriver() throws SQLException {
+        // default constructor
+        DriverManager.registerDriver(this);
+        this.active = true;
+    }
 
-	/** Stop intercepting requests.
-	 * @throws SQLException
-	 */
-	public void unregister() throws SQLException{
-		this.connection = null;
-		this.mockJDBCAnswer = null;
-		this.active = false;
-		DriverManager.deregisterDriver(this);
-	}
+    /**
+     * Stop intercepting requests.
+     * @throws SQLException
+     */
+    public void unregister() throws SQLException {
+        this.connection = null;
+        this.mockJDBCAnswer = null;
+        this.active = false;
+        DriverManager.deregisterDriver(this);
+    }
 
-	/** Connection handle to return
-	 * @param mockJDBCAnswer answer class
-	 * @throws SQLException 
-	 */
-	public MockJDBCDriver(MockJDBCAnswer mockJDBCAnswer) throws SQLException{
-		this();
-		this.mockJDBCAnswer = mockJDBCAnswer;
-	}
-	
-	/** Return the connection when requested.
-	 * @param connection
-	 * @throws SQLException
-	 */
-	public MockJDBCDriver(Connection connection) throws SQLException{
-		this();
-		this.connection = connection;
-	}
-	/** {@inheritDoc}
-	 * @see java.sql.Driver#acceptsURL(java.lang.String)
-	 */
-//	@Override
-	public synchronized boolean acceptsURL(String url) throws SQLException {
-		return this.active && url.startsWith("jdbc:mock"); // accept anything
-	}
+    /**
+     * Connection handle to return
+     * @param mockJDBCAnswer answer class
+     * @throws SQLException
+     */
+    public MockJDBCDriver(MockJDBCAnswer mockJDBCAnswer) throws SQLException {
+        this();
+        this.mockJDBCAnswer = mockJDBCAnswer;
+    }
 
-	/** {@inheritDoc}
-	 * @see java.sql.Driver#connect(java.lang.String, java.util.Properties)
-	 */
-	// @Override
-	public synchronized Connection connect(String url, Properties info) throws SQLException {
-		if (url.startsWith("invalid") || url.equals("")){
-			throw new SQLException("Mock Driver rejecting invalid URL");
-		}
-		if (this.connection != null){
-			return this.connection;
-		}
-		
-		if (this.mockJDBCAnswer == null){
-			return new MockConnection();
-		}
-		
-		return this.mockJDBCAnswer.answer();
-	}
+    /**
+     * Return the connection when requested.
+     * @param connection
+     * @throws SQLException
+     */
+    public MockJDBCDriver(Connection connection) throws SQLException {
+        this();
+        this.connection = connection;
+    }
 
-	/** {@inheritDoc}
-	 * @see java.sql.Driver#getMajorVersion()
-	 */
-	// @Override
-	public int getMajorVersion() {
-		return 1;
-	}
+    /**
+     * {@inheritDoc}
+     * @see java.sql.Driver#acceptsURL(java.lang.String)
+     */
+    // @Override
+    public synchronized boolean acceptsURL(String url) throws SQLException {
+        return this.active && url.startsWith("jdbc:mock"); // accept anything
+    }
 
-	/** {@inheritDoc}
-	 * @see java.sql.Driver#getMinorVersion()
-	 */
-	// @Override
-	public int getMinorVersion() {
-		return 0;
-	}
+    /**
+     * {@inheritDoc}
+     * @see java.sql.Driver#connect(java.lang.String, java.util.Properties)
+     */
+    // @Override
+    public synchronized Connection connect(String url, Properties info) throws SQLException {
+        if (url.startsWith("invalid") || url.equals("")) {
+            throw new SQLException("Mock Driver rejecting invalid URL");
+        }
+        if (this.connection != null) {
+            return this.connection;
+        }
+        if (this.mockJDBCAnswer == null) {
+            return new MockConnection();
+        }
+        return this.mockJDBCAnswer.answer();
+    }
 
-	/** {@inheritDoc}
-	 * @see java.sql.Driver#getPropertyInfo(java.lang.String, java.util.Properties)
-	 */
-	// @Override
-	public synchronized DriverPropertyInfo[] getPropertyInfo(String url, Properties info)
-			throws SQLException {
-		return new DriverPropertyInfo[0];
-	}
+    /**
+     * {@inheritDoc}
+     * @see java.sql.Driver#getMajorVersion()
+     */
+    // @Override
+    public int getMajorVersion() {
+        return 1;
+    }
 
-	/** {@inheritDoc}
-	 * @see java.sql.Driver#jdbcCompliant()
-	 */
-	// @Override
-	public boolean jdbcCompliant() {
-		return true;
-	}
+    /**
+     * {@inheritDoc}
+     * @see java.sql.Driver#getMinorVersion()
+     */
+    // @Override
+    public int getMinorVersion() {
+        return 0;
+    }
 
-  /* #ifdef JDK7
-//  @Override
-  public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-    return null;
-  }
-  #endif JDK7 */
+    /**
+     * {@inheritDoc}
+     * @see java.sql.Driver#getPropertyInfo(java.lang.String, java.util.Properties)
+     */
+    // @Override
+    public synchronized DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+        return new DriverPropertyInfo[0];
+    }
 
-  /**
-	 * Disable everything.
-	 * @throws SQLException 
-	 * @throws SQLException 
-	 */
-	public synchronized void disable() throws SQLException{
-		this.connection = null;
-		this.mockJDBCAnswer = null;
-		DriverManager.deregisterDriver(this);
-	}
+    /**
+     * {@inheritDoc}
+     * @see java.sql.Driver#jdbcCompliant()
+     */
+    // @Override
+    public boolean jdbcCompliant() {
+        return true;
+    }
 
-	/**
-	 * @return the connection
-	 */
-	public synchronized Connection getConnection() {
-		return this.connection;
-	}
+    // #ifdef JDK7
+    // @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
 
-	/**
-	 * @param connection the connection to set
-	 */
-	public synchronized  void setConnection(Connection connection) {
-		this.connection = connection;
-	}
+    // #endif JDK7 //
+    /**
+     * Disable everything.
+     * @throws SQLException
+     * @throws SQLException
+     */
+    public synchronized void disable() throws SQLException {
+        this.connection = null;
+        this.mockJDBCAnswer = null;
+        DriverManager.deregisterDriver(this);
+    }
 
-	/** Return the jdbc answer class
-	 * @return the mockJDBCAnswer
-	 */
-	public synchronized  MockJDBCAnswer getMockJDBCAnswer() {
-		return this.mockJDBCAnswer;
-	}
+    /**
+     * @return the connection
+     */
+    public synchronized Connection getConnection() {
+        return this.connection;
+    }
 
-	/** Sets the jdbc mock answer.
-	 * @param mockJDBCAnswer the mockJDBCAnswer to set
-	 */
-	public synchronized void setMockJDBCAnswer(MockJDBCAnswer mockJDBCAnswer) {
-		this.mockJDBCAnswer = mockJDBCAnswer;
-	}
+    /**
+     * @param connection the connection to set
+     */
+    public synchronized void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    /**
+     * Return the jdbc answer class
+     * @return the mockJDBCAnswer
+     */
+    public synchronized MockJDBCAnswer getMockJDBCAnswer() {
+        return this.mockJDBCAnswer;
+    }
+
+    /**
+     * Sets the jdbc mock answer.
+     * @param mockJDBCAnswer the mockJDBCAnswer to set
+     */
+    public synchronized void setMockJDBCAnswer(MockJDBCAnswer mockJDBCAnswer) {
+        this.mockJDBCAnswer = mockJDBCAnswer;
+    }
 }
